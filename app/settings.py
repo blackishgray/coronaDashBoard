1258880ee5d4122e97dc53a87b0eef5939446202
blackishgray <ratnadeep.g@somaiya.edu>
2021-06-02 17:37:49 +0530

    Final

--- app/settings.py
+++ app/settings.py
@@ -48,8 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.whitenoiseMiddleware',
+    'whitenoise.middleware.whiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
